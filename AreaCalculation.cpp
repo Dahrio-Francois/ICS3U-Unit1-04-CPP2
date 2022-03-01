1cf64cf24ae815d3d329cf3c585dcd580f73a1bd
Dahrio-Francois <dahrio.francois@mths.ca>
2022-03-01 19:19:10 +0000

    made a change in AreaCalculation.cpp

--- AreaCalculation.cpp
+++ AreaCalculation.cpp
@@ -3,17 +3,17 @@
 // Created by: Dahrio Francois
 // Created on: Feb 2022
 // This program calculates the area and perimeter of a rectangle
-//    with dimensions 5cm x 3cm
+//    with dimensions 6 cm x 4 cm
 
 #include <iostream>
 #include <cmath>
 
 int main() {
     std::cout << "If a rectangle has the dimensions: " << std::endl;
-    std::cout << "6cm x 4cm" << std::endl;
+    std::cout << "6 cm x 4 cm" << std::endl;
     std::cout << std::endl;
-    std::cout << "Area is " << (6 * 4) << "cm²." << std::endl;
-    std::cout << "Perimeter is " << (2 * (6 + 4)) << "cm." << std::endl;
+    std::cout << "Area is " << (6 * 4) << " cm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * (6 + 4)) << " cm." << std::endl;
     std::cout << std::endl;
     std::cout << "Done." << std::endl;
 }
